0205a1c580c71dd07aa1c9d0ba5f60d69334e4a5
rghvm <rghvmunjal@gmail.com>
2021-06-14 22:11:17 +0530

    fifth

---
+++ lib/story.dart
@@ -0,0 +1,13 @@
+class Story{
+
+  String title='';
+  String choice1='';
+  String choice2='';
+
+  Story(String t,String ch1,String ch2){
+    this.title=t;
+    this.choice1=ch1;
+    this.choice2=ch2;
+  }
+
+}
